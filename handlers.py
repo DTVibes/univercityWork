0c86fd43b6f7c1ccc096fa9930234526b0f730e1
DTVibes <bole78@list.ru>
2024-05-11 14:30:48 +0200

    Приближение

--- handlers.py
+++ handlers.py
@@ -68,7 +68,7 @@ def find_printer(message):
 
 def send_main_menu(chat_id, user_first_name):
     markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-    buttons = ['Скачать инструкцию', 'Выбрать оглавление', 'Функции', 'Описание принтера', 'Выбрать другой принтер']
+    buttons = ['Скачать инструкцию', 'Пользовательская инструкция', 'Функции', 'Описание принтера', 'Выбрать другой принтер']
     markup.add(*[types.KeyboardButton(btn) for btn in buttons])
     bot.send_message(chat_id, f"Привет, {user_first_name}! Чем я могу вам помочь?", reply_markup=markup)
 
@@ -78,7 +78,7 @@ def handle_main_menu(message):
     chat_id = message.chat.id
     if current_menu_item == 'Скачать инструкцию':
         handle_printer_link(message)
-    elif current_menu_item == 'Выбрать оглавление':
+    elif current_menu_item == 'Пользовательская инструкция':
         send_instruction_menu(chat_id)
     elif current_menu_item == 'Функции':
         pass  # Добавьте здесь логику для отображения функций принтера
@@ -96,11 +96,13 @@ def handle_printer_link(message):
         if printer_info:
             printer_name = printer_info[0][0]
             link = printer_info[0][1]
-            bot.send_message(chat_id, f"Ссылка на принтер *{printer_name}*: \n\n{link}", parse_mode='Markdown')
+            # Отправляем сообщение без форматирования
+            bot.send_message(chat_id, f"Ссылка на принтер {printer_name}: \n\n{link}")
         else:
             bot.send_message(chat_id, f"Принтер с id '{printer_id}' не найден в базе данных. Попробуйте снова.")
     else:
         bot.send_message(chat_id, "Не удалось определить принтер. Пожалуйста, выберите принтер заново.")
+
 
 def handle_printer_description(message):
     chat_id = message.chat.id
@@ -176,6 +178,10 @@ def handle_section_choice(message):
     else:
         bot.send_message(chat_id, "Не удалось определить принтер. Пожалуйста, выберите принтер заново.")
 
+
+
+
+
 def send_subsections_menu(chat_id, section_id, section_title):
     subsections = execute_query('SELECT id, title FROM Subsections WHERE id_section=?', (section_id,))
     if subsections:
@@ -183,13 +189,34 @@ def send_subsections_menu(chat_id, section_id, section_title):
         for subsection in subsections:
             button = types.KeyboardButton(subsection[1])
             markup.add(button)
+        markup.add(types.KeyboardButton("Назад"))  # Добавляем кнопку "Назад" после создания разметки
         bot.send_message(chat_id, f"Выберите подраздел для раздела '{section_title}':", reply_markup=markup)
-        bot.register_next_step_handler_by_chat_id(chat_id, handle_subsection_choice)
+        # Передаем chat_id, section_id и section_title в функцию handle_subsection_choice
+        bot.register_next_step_handler_by_chat_id(chat_id, handle_subsection_choice, section_id=section_id, section_title=section_title)
     else:
         bot.send_message(chat_id, f"Для раздела '{section_title}' нет доступных подразделов.")
 
 
-def handle_subsection_choice(message):
+
+
+
+def send_text_and_picture(chat_id, text, photo_data):
+    if text and photo_data:
+        # Если есть и текст, и фото, отправляем их вместе
+        message_text = f"{text}\n\n[Изображение]"
+        send_message_with_image(chat_id, message_text, photo_data)
+    elif text:
+        # Если есть только текст, отправляем его
+        bot.send_message(chat_id, text)
+    elif photo_data:
+        # Если есть только фото, отправляем его
+        send_message_with_image(chat_id, "[Изображение]", photo_data)
+    else:
+        # Если и текст, и фото отсутствуют, отправляем сообщение об этом
+        bot.send_message(chat_id, "Для выбранного подраздела нет текста или фото.")
+
+
+def handle_subsection_choice(message, section_id, section_title):
     chat_id = message.chat.id
     subsection_title = message.text
     printer_id = sessions.get(chat_id, {}).get('printer_id')
@@ -197,30 +224,66 @@ def handle_subsection_choice(message):
         subsection_info = execute_query('SELECT id FROM Subsections WHERE title=?', (subsection_title,))
         if subsection_info:
             subsection_id = subsection_info[0][0]
+            # Retrieve Subsections2 titles
+            subsections2_titles = execute_query('SELECT title FROM Subsections2 WHERE id_subsection=?', (subsection_id,))
+            if subsections2_titles:
+                markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)  # Установка row_width=2
+                buttons = [title[0] for title in subsections2_titles]
+                markup.add(*[types.KeyboardButton(btn) for btn in buttons])
+                markup.add(types.KeyboardButton("Назад"))
+                bot.send_message(chat_id, "Выберите подраздел второго уровня:", reply_markup=markup)
+                # Передаем section_id и section_title в функцию handle_subsection2_choice
+                bot.register_next_step_handler(message, handle_subsection2_choice, section_id=section_id, section_title=section_title)
+            else:
+                bot.send_message(chat_id, "Для выбранного подраздела нет доступных подразделов второго уровня.")
+        else:
+            bot.send_message(chat_id, "Выбранный подраздел не найден. Пожалуйста, выберите подраздел снова.")
+    else:
+        bot.send_message(chat_id, "Не удалось определить принтер. Пожалуйста, выберите принтер заново.")
+
+
+def handle_subsection2_choice(message, section_id, section_title):
+    chat_id = message.chat.id
+    subsection_title = message.text
+    if subsection_title == "Назад":
+        send_subsections_menu(chat_id, section_id, section_title)
+        return 
+
+    printer_id = sessions.get(chat_id, {}).get('printer_id')
+    if printer_id:
+        subsection_info = execute_query('SELECT id FROM Subsections2 WHERE title=?', (subsection_title,))
+        if subsection_info:
+            subsection_id = subsection_info[0][0]
             # Retrieve text from Steps table
-            steps = execute_query('SELECT text FROM Steps WHERE id_subsection=?', (subsection_id,))
+            steps = execute_query('SELECT text FROM Steps WHERE id_subsection2=?', (subsection_id,))
             text = steps[0][0] if steps else None
 
-            # Retrieve photo from StepPhotos table
-            photos = execute_query('SELECT photo FROM StepPhotos WHERE id_subsection=?', (subsection_id,))
-            photo_data = photos[0][0] if photos else None
+            # Retrieve photos from StepPhotos table
+            photos = execute_query('SELECT photo FROM StepPhotos WHERE id_subsection2=?', (subsection_id,))
+            photo_data_list = [photo[0] for photo in photos] if photos else None
 
-            if text or photo_data:
-                if text and photo_data:
-                    # Send message with both text and photo
-                    send_message_with_image(chat_id, f"Текст подраздела '{subsection_title}':\n\n{text}", photo_data)
-                elif text:
-                    # Send message with text only
-                    bot.send_message(chat_id, f"Текст подраздела '{subsection_title}':\n\n{text}")
-                elif photo_data:
-                    # Send message with photo only
-                    send_message_with_image(chat_id, f"Текст подраздела '{subsection_title}':", photo_data)
+            # Проверяем наличие текста и фото перед отправкой
+            if text is not None or photo_data_list:
+                # Отправляем текст с разметкой Markdown, только если он существует
+                if text is not None:
+                    bot.send_message(chat_id, f"Текст подраздела *{subsection_title}*: \n\n{text}", parse_mode='Markdown')
+
+                # Отправляем фотографии
+                if photo_data_list:
+                    for photo_data in photo_data_list:
+                        send_message_with_image(chat_id, "", photo_data)
+
+                # После отправки информации о текущем подразделе ждем следующего выбора пользователя
+                bot.send_message(chat_id, "Выберите еще один подраздел второго уровня или нажмите 'Назад':")
+                # Передаем section_id и section_title в функцию handle_subsection2_choice
+                bot.register_next_step_handler(message, handle_subsection2_choice, section_id=section_id, section_title=section_title)
             else:
                 bot.send_message(chat_id, "Для выбранного подраздела нет текста или фото.")
         else:
             bot.send_message(chat_id, "Выбранный подраздел не найден. Пожалуйста, выберите подраздел снова.")
     else:
         bot.send_message(chat_id, "Не удалось определить принтер. Пожалуйста, выберите принтер заново.")
+
 
 
 if __name__ == '__main__':
